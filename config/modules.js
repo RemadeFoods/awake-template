c2bac88e14ff6fc71545a1f430419ae8347453cf
RemadeFoods <71409637+RemadeFoods@users.noreply.github.com>
2020-09-16 15:48:36 -0700

    Update modules.js

--- config/modules.js
+++ config/modules.js
@@ -49,6 +49,6 @@ export const modulesSettings = {
     disable: process.env.NODE_ENV === 'development'
   },
   'google-gtag': {
-    id: siteConfig.googleAnalytics.id
+    id: siteConfig.googleAnalytics.246597207
   }
 }
