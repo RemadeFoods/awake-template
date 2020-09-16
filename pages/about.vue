40313ab1241a48bede96e98e8e5495e51789c8ea
RemadeFoods <71409637+RemadeFoods@users.noreply.github.com>
2020-09-16 15:21:48 -0700

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -2,106 +2,25 @@
   <div id="about-page" class="page-wrapper about-page content-page">
     <site-hero
       title="About Us"
-      subtitle="Example About Page"
+      subtitle="Why Are We Doing This"
       image="/uploads/about-hero.jpg"
     ></site-hero>
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <div class="content">
-          <h3>Fake Heading</h3>
+          <h3>About Remade Foods</h3>
           <p>
-            <strong>
-              Edit this page in<code>/pages/about.vue</code>to fit your needs.
-            </strong>
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit!
-          </p>
-
-          <h3>Another Fake Heading</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit!
-          </p>
-          <quote-card />
-          <h3>Still More Fake Content</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit! Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit.
-            <strong>Alias aut facere harum ipsam</strong> quibusdam quidem
-            quisquam saepe? Alias architecto blanditiis consequatur consequuntur
-            dicta ipsa iste modi provident, quod, similique veritatis.
-          </p>
-        </div>
-
-        <div class="tile is-ancestor">
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                Cool Stuff We Do
-              </p>
-              <p class="subtitle">
-                Sooo Cool
-              </p>
-              <figure class="image is-1by1">
-                <opti-image width="500" height="500" />
-              </figure>
-            </article>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                Other Cool Things
-              </p>
-              <p class="subtitle">
-                Just as cool
-              </p>
-              <figure class="image is-1by1">
-                <opti-image width="500" height="500" />
-              </figure>
-            </article>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                All the cool
-              </p>
-              <p class="subtitle">
-                that is all
-              </p>
-              <figure class="image is-1by1">
-                <opti-image width="500" height="500" />
-              </figure>
-            </article>
-          </div>
-        </div>
-      </template>
-
-      <template v-slot:sidebar>
-        <h3 class="subtitle is-4">
-          Latest Posts
-        </h3>
-        <posts-grid :per-row="1" :number="2" />
-      </template>
-    </main-section>
-  </div>
-</template>
-<script>
-import QuoteCard from '~/components/cards/QuoteCard'
-export default {
-  head() {
-    return {
-      title: `About | ${this.$siteConfig.siteName}`
-    }
-  },
-  components: { QuoteCard }
-}
-</script>
+            Growing up we didn't have much. Our parents would always push us to finish everything on our plates. They would go so far as say that for every grain of rice we did not finish there would be a new pimple on our face in the morning. I remember there was one night I did not want to eat my vegetables for dinner and cunningly slipped them into the garbage can with other wrappers and "garbage" placed on top. About 10 minutes later my dad runs over screaming at me saying that I should not have wasted food! All I could do was sit and contemplate how on earth he found my hidden veggies.
+<p>
+</p>
+Now as an adult living in the crazy times of COVID-19 I am appreciating all the lessons I learned as a child. Food is now scarce and expensive. There is a shortage of pork. There are salmonella warnings on onions. Lettuce is being recalled. And I have to wear gloves and mask, risking my life as well as my family's to buy these groceries. We are in crazy times. 
+<p>
+</p>
+Now more than ever, we wanted to bring a dedicated website to centrally host recipes to help us remake leftovers from our refrigerators in an effort to reuse food and limit waste. I know so many people who choose to throw away their leftovers because they are sick of eating the same things or don't know how to reimagine what these leftovers could be. But with costs rising, these leftovers can save us so much money, time, and energy if we could just remake them into something more desirable. 
+<p>
+</p>
+Follow us and our growing collection of recipes as we embark on a new adventure, reimagining leftovers from 2 nights ago from the restaurant, parent's house, or anything we couldn't finish. Some of these recipes are from my childhood. Some are from restaurants trying to stretch a dollar. And some are from pure luck as we just refuse to throw leftovers away and try to make something delicious instead. Come back as often as you'd like and hopefully we have saved a leftover meal.
+<p>
+</p>
