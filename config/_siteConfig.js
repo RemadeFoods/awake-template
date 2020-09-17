2a9c5cca07135d4f546fdc9443d1adcf62e18df1
RemadeFoods <71409637+RemadeFoods@users.noreply.github.com>
2020-09-16 20:11:07 -0700

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -15,7 +15,7 @@ export default {
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
-    id: process.env.GOOGLE_ANALYTICS_ID
+    id: G-6YPL2SFMCH
   },
   mainMenu: [
     {
